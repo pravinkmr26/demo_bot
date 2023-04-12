1070d5bb619b6fdbb219f90f3bb85c1dae9bee69
Pravinkumar <pravinkmr2619@gmail.com>
2023-04-12 17:02:41 +0530

    Add 3d lidar

--- launch/launch_gaz.launch.py
+++ launch/launch_gaz.launch.py
@@ -1,13 +1,13 @@
-import os
-
 from ament_index_python.packages import get_package_share_directory
-
 
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+from launch_ros.substitutions import FindPackageShare
+from os.path import join
 
 
 
@@ -18,19 +18,32 @@ def generate_launch_description():
     # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
 
     package_name='demo_bot' 
+    
+    pkg_sub = get_package_share_directory('demo_bot')
+    
+    world_path=join(pkg_sub, 'worlds/gazebo_world.world')
 
     rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
+                PythonLaunchDescriptionSource([join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
+                PythonLaunchDescriptionSource([join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-             )
+             launch_arguments={'world': world_path}.items())
 
+    # spawn_entity.py [-h]
+    # [spawn_entity.py-4]                        (-file FILE_NAME | -topic TOPIC_NAME | -database ENTITY_NAME | -stdin)
+    # [spawn_entity.py-4]                        -entity ENTITY_NAME [-reference_frame REFERENCE_FRAME]
+    # [spawn_entity.py-4]                        [-gazebo_namespace GAZEBO_NAMESPACE]
+    # [spawn_entity.py-4]                        [-robot_namespace ROBOT_NAMESPACE] [-timeout TIMEOUT]
+    # [spawn_entity.py-4]                        [-unpause] [-wait ENTITY_NAME]
+    # [spawn_entity.py-4]                        [-spawn_service_timeout TIMEOUT] [-x X] [-y Y] [-z Z]
+    # [spawn_entity.py-4]                        [-R R] [-P P] [-Y Y] [-package_to_model] [-b]
+    
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
@@ -41,7 +54,10 @@ def generate_launch_description():
 
     # Launch them all!
     return LaunchDescription([
-        rsp,
+        # ExecuteProcess(
+        #     cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so', world_path], 
+        #     output='screen'),
         gazebo,
-        spawn_entity,
+        rsp,
+        spawn_entity
     ])
